3eb88f90e70b95123cb47a233901e62473594c7a
Radha Mohan <116169863+radhe30@users.noreply.github.com>
2026-02-24 20:28:57 +0530

    Modify app.run to set port from environment variable
    Update app.run to use environment variable for port

--- app.py
+++ app.py
@@ -35,5 +35,11 @@ def check_transaction():
     })
 
 
+#if __name__ == "__main__":
+ #   app.run(debug=True)
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
